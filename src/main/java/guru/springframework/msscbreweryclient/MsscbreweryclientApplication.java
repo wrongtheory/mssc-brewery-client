0678e8eb75fee8949ed9d3e8c9bbb6b381b7e70a
vleunti@gmail.com <M@h@gon5626256>
2021-01-27 02:50:56 +0100

    init

---
+++ src/main/java/guru/springframework/msscbreweryclient/MsscbreweryclientApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscbreweryclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscbreweryclientApplication.class, args);
+    }
+
+}
